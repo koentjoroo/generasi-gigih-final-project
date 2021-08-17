0a6e37dba9806f3d7cd1fbaa883a787e9b29e363
Novan <enerka4ig@gmail.com>
2021-08-17 16:07:55 +0700

    feat: add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import { Box, Flex, Grid, Heading, Text, Center, Image, Button } from '@chakra-ui/react'
+// import { FaSpotify } from 'react-icons/fa'
 
 function App() {
   return (
-    <div>App</div>
+    <Box maxW="100vw" p="2rem">
+      <Flex justify={'space-between'} align={'center'}>
+        <Text fontSize={'xl'} fontWeight={900}>GenGIGIH</Text>
+        <Button>Login with Spotify</Button>
+      </Flex>
+      <Grid templateColumns="1fr 1fr" gap="5vw" minH="100vh">
+        <Flex direction={'column'} justify={'center'}>
+          <Heading as={'h1'} fontSize={'6xl'}>Dengerim Musik Asek Lorem Ipsum Dot Color Si Aemt</Heading>
+          <Text my={4} color={'gray.400'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique nobis atque fuga, quasi omnis odit sunt placeat magni voluptate ratione incidunt quas ad blanditiis, soluta vitae aut velit. Magni.</Text>
+          <Box>
+            <Button size={'lg'}>Login with Spotify</Button>
+          </Box>
+        </Flex>
+        <Center>
+          <Image src={'https://picsum.photos/500'} />
+        </Center>
+      </Grid>
+    </Box>
   );
 }
 
