e14391737d3be1e0c412ba8ce4c8c2ed292834d2
Novan <enerka4ig@gmail.com>
2021-08-20 04:11:35 +0700

    refactor: main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import Landing from './pages/landing'
+import NewReleases from './pages/new-releases'
 import CreatePlaylist from './pages/create-playlist'
 import Header from './components/header'
+import Sidebar from './components/sidebar'
+import { Grid } from '@chakra-ui/react'
 import { useAppSelector } from './store'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
@@ -10,17 +13,20 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <main>
-        {/* <Sidebar /> */}
+      <Grid as={'main'} templateColumns={isAuthenticated ? '1fr 4fr' : '1fr'}>
+        {isAuthenticated && <Sidebar />}
         <Switch>
           <Route exact path="/create-playlist">
             {isAuthenticated ? <CreatePlaylist /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/new-releases">
+            {isAuthenticated ? <NewReleases /> : <Redirect to="/" />}
           </Route>
           <Route exact path="/">
             <Landing />
           </Route>
         </Switch>
-      </main>
+      </Grid>
     </BrowserRouter>
   )
 }
