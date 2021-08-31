fdc40b0502863bbf56be9555475a5bf0402f1462
Novan <enerka4ig@gmail.com>
2021-09-01 02:21:33 +0700

    fix: typo in landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text, Link, Center, Button, useColorMode } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text, Link, Button, useColorMode } from '@chakra-ui/react'
 import { FaSpotify } from 'react-icons/fa'
 import { authorize } from '../libs/spotify'
 import { useAuth } from '../libs/use-auth'
@@ -12,7 +12,7 @@ const Landing = () => {
     <Box maxW="100vw">
       <Flex as={'main'} minH="100vh" p={'2rem'} direction={'column'} justify={'center'} textAlign={'center'}>
         { landingImage }
-        <Heading as={'h1'} fontSize={'6xl'}>Dengerim Musik Asik</Heading>
+        <Heading as={'h1'} fontSize={'6xl'}>Dengerin Musik Asik</Heading>
         <Text my={4} color={'gray.400'}>Tau gak dengerin musik bisa bikin mood kamu jadi lebih baik loh! Yuks bikin playlist kamu dari sini!</Text>
         <Box>
           <Button size={'lg'} leftIcon={<FaSpotify />} onClick={authorize}>Login with Spotify</Button>
