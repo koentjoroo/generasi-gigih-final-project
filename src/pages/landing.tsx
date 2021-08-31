34b3417799eac84b8cda3c039f77251a0ccf87e9
Novan <enerka4ig@gmail.com>
2021-09-01 02:26:21 +0700

    fix: landing page image and footer

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text, Link, Button, useColorMode } from '@chakra-ui/react'
+import { Box, Flex, Center, Heading, Text, Link, Button, Image, useColorMode } from '@chakra-ui/react'
 import { FaSpotify } from 'react-icons/fa'
 import { authorize } from '../libs/spotify'
 import { useAuth } from '../libs/use-auth'
@@ -11,17 +11,17 @@ const Landing = () => {
   return (
     <Box maxW="100vw">
       <Flex as={'main'} minH="100vh" p={'2rem'} direction={'column'} justify={'center'} textAlign={'center'}>
-        { landingImage }
+        <Image src={landingImage} alt="Listening Happy Music Animation" />
         <Heading as={'h1'} fontSize={'6xl'}>Dengerin Musik Asik</Heading>
         <Text my={4} color={'gray.400'}>Tau gak dengerin musik bisa bikin mood kamu jadi lebih baik loh! Yuks bikin playlist kamu dari sini!</Text>
         <Box>
           <Button size={'lg'} leftIcon={<FaSpotify />} onClick={authorize}>Login with Spotify</Button>
         </Box>
       </Flex>
-      <Flex as={'footer'} h={'10vh'} bg={colorMode === 'light' ? 'gray.300' : 'gray.800'}  alignContent={'flex-end'}>
+      <Center as={'footer'} h={'10vh'} bg={colorMode === 'light' ? 'gray.300' : 'gray.800'} >
         <Text mr={4}>Made with 💖 by Novan</Text>
         <Link href="https://storyset.com/people" isExternal>People illustrations by Storyset</Link>
-      </Flex>
+      </Center>
     </Box>
   )
 }
