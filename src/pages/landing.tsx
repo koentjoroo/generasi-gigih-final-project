5fb3bc6970d5cd64c094fa9b69501ea00f0d76f8
Novan <enerka4ig@gmail.com>
2021-09-01 02:51:12 +0700

    fix: landing page layout

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,4 +1,16 @@
-import { Box, Flex, Center, Heading, Text, Link, Button, Image, useColorMode } from '@chakra-ui/react'
+import {
+  Box,
+  Grid,
+  Flex,
+  Center,
+  Heading,
+  Text,
+  Link,
+  Button,
+  Image,
+  useColorMode,
+  useBreakpointValue
+} from '@chakra-ui/react'
 import { FaSpotify } from 'react-icons/fa'
 import { authorize } from '../libs/spotify'
 import { useAuth } from '../libs/use-auth'
@@ -8,19 +20,49 @@ const Landing = () => {
   const { colorMode } = useColorMode()
   useAuth()
 
+  const column = useBreakpointValue({ base: '1fr', md: '1fr 1fr'})
+  const row = useBreakpointValue({ base: '1fr 1fr', md: '1fr'})
+
   return (
     <Box maxW="100vw">
-      <Flex as={'main'} minH="100vh" p={'2rem'} direction={'column'} justify={'center'} textAlign={'center'}>
-        <Image width={[300, 400, 500]} src={landingImage} alt="Listening Happy Music Animation" />
-        <Heading as={'h1'} fontSize={'6xl'}>Dengerin Musik Asik</Heading>
-        <Text my={4} color={'gray.400'}>Tau gak dengerin musik bisa bikin mood kamu jadi lebih baik loh! Yuks bikin playlist kamu dari sini!</Text>
-        <Box>
-          <Button size={'lg'} leftIcon={<FaSpotify />} onClick={authorize}>Login with Spotify</Button>
-        </Box>
-      </Flex>
-      <Center as={'footer'} h={'10vh'} bg={colorMode === 'light' ? 'gray.300' : 'gray.800'} >
+      <Grid templateColumn={column} templateRow={row} gap={'1rem'}>
+        <Center>
+          <Image
+            width={[300, 400, 500]}
+            src={landingImage}
+            alt="Listening Happy Music Animation"
+          />
+        </Center>
+        <Flex
+          as={'main'}
+          minH="100vh"
+          p={'2rem'}
+          direction={'column'}
+          justify={'center'}
+        >
+          <Heading as={'h1'} fontSize={'6xl'}>
+            Dengerin Musik Asik
+          </Heading>
+          <Text my={4} color={'gray.400'}>
+            Tau gak dengerin musik bisa bikin mood kamu jadi lebih baik loh!
+            Yuks bikin playlist kamu dari sini!
+          </Text>
+          <Box>
+            <Button size={'lg'} leftIcon={<FaSpotify />} onClick={authorize}>
+              Login with Spotify
+            </Button>
+          </Box>
+        </Flex>
+      </Grid>
+      <Center
+        as={'footer'}
+        h={'10vh'}
+        bg={colorMode === 'light' ? 'gray.300' : 'gray.800'}
+      >
         <Text mr={4}>Made with 💖 by Novan</Text>
-        <Link href="https://storyset.com/people" isExternal>People illustrations by Storyset</Link>
+        <Link href="https://storyset.com/people" isExternal>
+          People illustrations by Storyset
+        </Link>
       </Center>
     </Box>
   )
